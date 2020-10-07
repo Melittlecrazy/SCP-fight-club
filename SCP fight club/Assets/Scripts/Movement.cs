9ce834a4d09a4b7e03efc8817c8896a518c161b5
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-10-06 21:07:26 -0500

    Added movement, specifically moving left and right
    there is still some slight draw back cause gravity and needs to be reduced.

---
+++ SCP fight club/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveSidetoSide = Input.GetAxis("Horizontal");
+        Vector2 inputDirection = new Vector2(moveSidetoSide, 5 * speed);
+        rigidbody.AddForce(inputDirection * speed);
+
+        if (Input.GetKey(Space)) ;
+    }
+}
